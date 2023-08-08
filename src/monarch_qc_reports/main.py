4340ddb943c0e00fde1946fb40576d26089059dd
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2023-08-08 09:54:51 -0500

    Initial commit

---
+++ src/monarch_qc_reports/main.py
@@ -0,0 +1,10 @@
+"""Main python file."""
+
+
+def demo():
+    """Define API."""
+    print("Hello, Corey!")
+
+
+if __name__ == "__main__":
+    demo()
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Monarch_QC_Reports."""
